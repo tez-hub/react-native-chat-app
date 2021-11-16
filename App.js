e97711e1643ebc2b1f30a48c6150b63b2772ed17
tez-hub <dennismwema8@gmail.com>
2021-11-16 05:37:18 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import signupScreen from './screens/signupScreen';
+import loginScreen from './screens/loginScreen';
+import loadingScreen from './screens/loadingScreen';
+import homeScreen from './screens/homeScreen';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './config';
+import introScreen from './screens/introScreen';
+import introPreScreen from './screens/introPreScreen';
+
+firebase.initializeApp(firebaseConfig);
+
+const myswitch = createSwitchNavigator({
+    stack: createStackNavigator({
+      inroPre: introPreScreen,
+      intro: introScreen,
+      login: loginScreen,  
+      signup: signupScreen,
+      
+  },{
+    defaultNavigationOptions: {
+      headerTintColor: 'white',
+      headerTitleContainerStyle:{
+        justifyContent:'flex-start',
+        elevation:9,
+      },
+      headerTitleStyle:{
+        fontSize:35,
+        fontWeight:'bold',
+        padding:10,
+        paddingTop:30,
+        paddingBottom:30,
+      },
+      headerStyle: {
+        backgroundColor: 'orange',
+      },
+    },
+  }),
+  home: homeScreen,
+  loading: loadingScreen,
+  
+});
+
+export default createAppContainer(myswitch);
+
---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "XXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXX",
+  measurementId: "XXXXXXX"
+};
